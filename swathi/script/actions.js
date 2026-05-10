420c2d8891b51f71db19b092322b127c9113722b
swathikanuganti94 <swathikanuganti94@gmail.com>
2026-05-10 16:09:16 +0530

    swathi changes

---
+++ swathi/script/actions.js
@@ -0,0 +1,13 @@
+var x = 6;
+
+function changeColor() {
+  document.getElementById("song").style.color="blue";
+  // comment. This will display the message on console.
+  console.log(x); 
+}
+
+function changeColorOnMouseOver() {
+  document.getElementById("song").style.color="orange";
+  // comment. This will display the message on console.
+  console.log(`value of x is ${x}`); 
+}
